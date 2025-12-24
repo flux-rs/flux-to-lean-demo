2db04db66f135e1c5bfc80863963536ab38e287b
Ranjit Jhala <rjhala@ucsd.edu>
2025-12-23 22:03:22 -0800

    ok clippy

--- LeanUserProofs/Proof/FibSumLoopProof.lean
+++ LeanUserProofs/Proof/FibSumLoopProof.lean
@@ -1,15 +1,21 @@
 import LeanProofs.Flux.Prelude
 import LeanProofs.Flux.VC.FibSumLoop
 
--- @[simp]
--- def k0 (i : Int) (total : Int) (n : Int) : Prop :=
---   0 <= i /\ i <= n /\ total = fib_spec_sum i
+@[simp]
+def k0 (i : Int) (total : Int) (n : Int) : Prop :=
+  0 <= i /\ i <= n /\ total = fib_spec_sum i
 
 def FibSumLoop_proof : FibSumLoop := by
   unfold FibSumLoop
+  exists k0
   simp
   intros
-  and_intros <;> try unfold fib_spec_sum <;> intros
-  all_goals
-    try (unfold fib_spec_sum)
-    grind
+  and_intros
+  . grind
+  . unfold fib_spec_sum; grind
+  . intros <;> and_intros
+    . intros; grind
+    . intros <;> and_intros
+      . grind
+      . grind
+      . unfold fib_spec_sum; grind
