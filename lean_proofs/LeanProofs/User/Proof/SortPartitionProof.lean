a8fb6e7c1e96922d9adfd25ae6eecbdf329415ce
jam-khan <jamkhan@connect.hku.hk>
2026-04-26 14:07:08 +0800

    cleanup in-progress

--- lean_proofs/LeanProofs/User/Proof/SortPartitionProof.lean
+++ lean_proofs/LeanProofs/User/Proof/SortPartitionProof.lean
@@ -1,7 +1,8 @@
 import LeanProofs.Flux.Prelude
 import LeanProofs.Flux.VC.SortPartition
 import LeanProofs.Lib.Lemmas
-import LeanProofs.Lib.Tactics
+-- import LeanProofs.Lib.Tactics
+import LeanFixpoint
 
 open Classical
 
@@ -9,7 +10,7 @@ namespace F
 
 -- ((k1 j₀ (VectorsAVec.elems a'₂) (VectorsAVec.len a'₂) i₀ (VectorsAVec.elems old₀) (VectorsAVec.len old₀) lo₀ hi₀)) ->
 
-@[reducible]
+@[simp]
 def part_inv (j: Int) (arr : Arr Int) (arr_len i : Int) (old_elems: Arr Int) (old_len lo hi: Int) :=
      arr_len == old_len
   /\ 0 <= lo /\ hi < arr_len /\ lo <= i /\ i <= hi
@@ -25,35 +26,7 @@ def SortPartition_proof : SortPartition := by
   unfold SortPartition
   exists k0; exists part_inv; exists k2; exists k3
   simp [LeanProofs.Lib.Lemmas.arr_get, LeanProofs.Lib.Lemmas.arr_set, sort_is_perm, is_frame, is_perm, part_inv, k0, k2, k3]
-  zap
-  . rename_i old lo hi _ _ _ _ _ _ _ _ _ _ _ _ j arr i _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ arr' _ h
-    obtain ⟨ old_def , _ ⟩ := h
-    . rw [old_def]; grind
-    . grind
-  . -- perm
-    rename_i old lo hi _ _ _ _ _ _ _ _ _ _ _ _  j arr i _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  arr' i' h  ix _ _
-    obtain ⟨ old_def , _ ⟩ := h
-    . rw [old_def]; grind
-    . grind
-  . -- frame ix < i
-    rename_i old lo hi _ _ _ _ _ _ _ _ _ _ _ _ j arr i _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  arr' _ h ix _
-    obtain ⟨ old_def , _ ⟩ := h
-    . rw [old_def]; grind
-    . grind
-  . -- frame hi < *
-    rename_i old lo hi _ _ _ _ _ _ _ _ _ _ _ _ j arr i _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  arr' _ h ix _
-    obtain ⟨ old_def , _ ⟩ := h
-    . rw [old_def]; grind
-    . grind
-  . -- lo <= ix < i -> arr ix <= pivot
-    rename_i old lo hi _ _ _ _ _ _ _ _ _ _ _ _ j arr i _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ arr' i' h ix _ _
-    obtain ⟨ old_def , _ ⟩ := h
-    . rw [old_def]; grind
-    . grind
-  . -- i <= ix < j -> pivot <= arr ix
-    rename_i old lo hi _ _ _ _ _ _ _ _ _ _ _ _ j arr i _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ arr' i' h ix _ _
-    obtain ⟨ old_def , _ ⟩ := h
-    . rw [old_def]; grind
-    . grind
+  solve_fixpoint
+
 
 end F
