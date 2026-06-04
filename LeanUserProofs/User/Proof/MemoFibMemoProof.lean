c458a11924b534571fb3f1f7076ce2e1e1053c0f
Ranjit Jhala <rjhala@ucsd.edu>
2026-06-04 06:34:38 -0700

    fix fibmemoproof

--- LeanUserProofs/User/Proof/MemoFibMemoProof.lean
+++ LeanUserProofs/User/Proof/MemoFibMemoProof.lean
@@ -9,9 +9,6 @@ namespace F
 
 def MemoFibMemo_proof : MemoFibMemo := by
   unfold MemoFibMemo
-  exists MemoFibMemoKVarSolutions.k0
-  exists MemoFibMemoKVarSolutions.k1
-  simp [MemoFibMemoKVarSolutions.k0, MemoFibMemoKVarSolutions.k1]
   zap
 
 end F
