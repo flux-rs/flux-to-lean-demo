a490dc56f5148db62970b5425b1f9d5db2c52957
Petros Markopoulos <petrosmarkopoulos01@gmail.com>
2026-06-28 16:28:15 -0700

    get should have a definition

--- LeanUserProofs/User/Fun/RingbufferFsliceGet.lean
+++ LeanUserProofs/User/Fun/RingbufferFsliceGet.lean
@@ -6,7 +6,9 @@ set_option linter.unusedVariables false
 
 namespace F
 
-noncomputable def ringbuffer_fslice_get : {t0 : Type} -> [Inhabited t0] -> (FSlice t0) -> Int -> t0 := sorry
+@[grind]
+noncomputable def ringbuffer_fslice_get : {t0 : Type} -> [Inhabited t0] -> (FSlice t0) -> Int -> t0 :=
+  fun s i => s[i.toNat]!
 
 
 end F
