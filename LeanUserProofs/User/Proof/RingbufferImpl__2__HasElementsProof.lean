930999bc142fbc2478bddd612d8c25d37e2e20b2
Petros Markopoulos <petrosmarkopoulos01@gmail.com>
2026-06-28 11:11:16 -0700

    Add specs to track exact elements and prove it models a queue

---
+++ LeanUserProofs/User/Proof/RingbufferImpl__2__HasElementsProof.lean
@@ -0,0 +1,13 @@
+import LeanProofs.Flux.Prelude
+import LeanProofs.Flux.VC.RingbufferImpl__2__HasElements
+open Classical
+set_option linter.unusedVariables false
+
+
+namespace F
+
+def RingbufferImpl__2__HasElements_proof : RingbufferImpl__2__HasElements := by
+  unfold RingbufferImpl__2__HasElements
+  grind
+
+end F
